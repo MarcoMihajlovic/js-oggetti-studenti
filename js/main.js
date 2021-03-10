ac8c559244063d047e6197e4cc2353311b335ec8
Marco <marcosinisa.mihajlovic@gmail.com>
2021-03-10 14:05:50 +0100

    Seconda Parte

--- js/main.js
+++ js/main.js
@@ -1,9 +1,46 @@
 var studente = {
-    'nome' : 'Marco',
-    'cognome' : 'Mihajlovic',
-    'eta' : 26
+    'nome': 'Marco',
+    'cognome': 'Mihajlovic',
+    'eta': 26
 };
 
 for (var key in studente) {
     console.log( key + ": " + studente[key]);
+}
+
+var studenti = [
+    {
+        'nome': 'Marco',
+        'cognome': 'Mihajlovic',
+        'eta': 26
+    },
+    
+    {
+        'nome': 'Hercule',
+        'cognome': 'Poirot',
+        'eta': 60
+    },
+    
+    {
+        'nome': 'Sherlock',
+        'cognome': 'Holmes',
+        'eta': 60
+    },
+
+    {
+        'nome': 'Jules',
+        'cognome': 'Maigret',
+        'eta': 53
+    },
+
+    {
+        'nome': 'Nero',
+        'cognome': 'Wolfe',
+        'eta': 56
+    }];
+
+for (var i = 0; i < studenti.length; i++){
+    console.log("");
+    console.log("Nome: " + studenti[i].nome);
+    console.log("Cognome: " + studenti[i].cognome);
 }
