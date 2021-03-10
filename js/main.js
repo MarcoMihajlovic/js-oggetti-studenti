97d8c1b4ec11175c9513994a5f534a1776abc039
Marco <marcosinisa.mihajlovic@gmail.com>
2021-03-10 14:58:28 +0100

    Aggiunti controlli su input

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ var studente = {
 };
 
 for (var key in studente) {
-    console.log( key + ": " + studente[key]);
+    console.log(key + ": " + studente[key]);
 }
 
 var studenti = [
@@ -45,21 +45,52 @@ for (var i = 0; i < studenti.length; i++){
     console.log("Cognome: " + studenti[i].cognome);
 }
 
-var nome = prompt("Inserisci il tuo nome");
-var cognome = prompt("Inserisci il tuo cognome");
-var eta = prompt("Inserisci la tua età");
+function insertStudent() {
+    var controllo = false;
 
-var obj = {
-    'nome': nome,
-    'cognome': cognome,
-    'eta': eta
-};
+    while(!controllo) {
+        var nome = prompt("Inserisci il tuo nome");
+        if(!isNaN(parseInt(nome)) || nome == "") {
+            alert("Devi inserire il tuo nome");
+        } else {
+            controllo = true;
+        }
+    }
+    
+    controllo = false;
+    while(!controllo) {
+        var cognome = prompt("Inserisci il tuo cognome");
+        if(!isNaN(parseInt(cognome)) || cognome == "") {
+            alert("Devi inserire il tuo cognome");
+        } else {
+            controllo = true;
+        }
+    }
 
-studenti.push(obj);
+    controllo = false;
+    while(!controllo) {
+        var eta = prompt("Inserisci la tua età");
+        if(isNaN(parseInt(eta))) {
+            alert("Devi inserire la tua eta'");
+        } else {
+            controllo = true;
+        }
+    }
 
-for (var i = 0; i < studenti.length; i++){
+    var obj = {
+        'nome': nome,
+        'cognome': cognome,
+        'eta': eta
+    };
+
+    studenti.push(obj);
     console.log("");
-    console.log("Nome: " + studenti[i].nome);
-    console.log("Cognome: " + studenti[i].cognome)
-    console.log("Eta: " + studenti[i].eta);
+    console.log("NUOVO STAMP");
+
+    for (var i = 0; i < studenti.length; i++){
+        console.log("");
+        console.log("Nome: " + studenti[i].nome);
+        console.log("Cognome: " + studenti[i].cognome)
+        console.log("Eta: " + studenti[i].eta);
+    }
 }
